93667c960af344dfce64aa9b0aca76af9b46192b
Berkay Emre Keskin <berkayemrekeskin@gmail.com>
2024-05-03 18:07:51 +0300

    command ! added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from typing import Final
 import os
+from typing import Final
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
 from responses import get_response
+from discord.ext import commands
 
 # Load token
 load_dotenv()
@@ -11,7 +12,8 @@ TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 # Bot setup
 intents : Intents = Intents.default()
 intents.message_content = True
-client = Client = Client(intents=intents)
+command_prefix = '!'
+client : commands = commands.Bot(command_prefix=command_prefix, intents=intents)
 
 # Message
 async def send_message(message: Message, user_message: str) -> None:
@@ -22,11 +24,12 @@ async def send_message(message: Message, user_message: str) -> None:
     if is_private := user_message[0] == '?':
         user_message = user_message[1:]
         
-    try:
-        response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
+    if is_command := user_message[0] == '!':
+        try:
+            response: str = get_response(user_message)
+            await message.author.send(response) if is_private else await message.channel.send(response)
+        except Exception as e:
+            print(e)
         
 # Handling startup
 @client.event
